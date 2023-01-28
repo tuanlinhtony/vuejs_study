9a13f8963fd72bb63eb090ffe256829ded52f92d
tuanlinhtony <mrpi3990@gmail.com>
2023-01-28 23:02:46 +0700

    Re-building the App with Vue

---
+++ Rebuilt app with Vue/app.prod.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue
+createApp({
+    data(){
+        return{
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
